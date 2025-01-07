264431c332f6b36e9403d84b8f0c388c299c2dbb
IurcoEgor <160418653+IurcoEgor@users.noreply.github.com>
2025-01-07 22:45:16 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,41 @@
+// Data
 var data = new Date();
 document.getElementById('an').innerHTML = data.getFullYear();
 
+//  Defilarea
 document.querySelectorAll('aside a').forEach(link => {
     link.addEventListener('click', (event) => {
         event.preventDefault();
         const targetId = link.getAttribute('href');
         const targetElement = document.querySelector(targetId);
         if (targetElement) {
+            const { offsetTop } = targetElement;
             window.scrollTo({
-                top: targetElement.offsetTop - 78,
+                top: offsetTop - 78,
                 behavior: 'smooth'
             });
         }
     });
 });
+
+//  Meniu mobil
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+if (hamburgerMenu) {
+    hamburgerMenu.addEventListener('click', () => {
+        document.querySelector('.nav-links').classList.toggle('show');
+    });
+}
+
+//Acasa
+const homeMenu = document.querySelector('.home-menu');
+const navHome = document.querySelector('.nav-home');
+
+if (homeMenu && navHome) {
+    const showMenu = () => navHome.classList.add('show');
+    const hideMenu = () => navHome.classList.remove('show');
+
+    homeMenu.addEventListener('mouseover', showMenu);
+    homeMenu.addEventListener('mouseout', hideMenu);
+    navHome.addEventListener('mouseover', showMenu);
+    navHome.addEventListener('mouseout', hideMenu);
+}

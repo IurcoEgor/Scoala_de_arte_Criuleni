f2c727585644554dae37399f14ee54a89553687d
IurcoEgor <160418653+IurcoEgor@users.noreply.github.com>
2025-02-20 00:13:09 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,63 @@
-
 // Data
 var data = new Date();
 document.getElementById('an').innerHTML = data.getFullYear();
+
+//  Meniu mobil
+
+const list = document.querySelectorAll('.navList');
+function activeLink() {
+    list.forEach((item) =>
+        item.classList.remove('active'));
+    this.classList.add('active');
+}
+list.forEach((item) =>
+    item.addEventListener('click', activeLink));
+
+document.addEventListener("DOMContentLoaded", function () {
+    const navItems = document.querySelectorAll(".navMenu ul li");
+    const indicator = document.querySelector(".indicator");
+    const navList = document.querySelector(".navMenu ul");
+
+    function updateIndicator() {
+        const activeItem = document.querySelector(".navMenu ul li.active");
+
+        if (activeItem && navList) {
+            const icon = activeItem.querySelector(".icon i");
+            const navRect = navList.getBoundingClientRect();
+            const activeItemRect = activeItem.getBoundingClientRect();
+            const iconRect = icon.getBoundingClientRect();
+
+            const iconCenter = iconRect.left - navRect.left + iconRect.width / 2;
+
+            const navListWidth = navList.offsetWidth;
+            const navListHeight = navList.offsetHeight;
+
+            const indicatorSize = Math.min(navListWidth, navListHeight);
+
+            indicator.style.width = `${indicatorSize}px`;
+            indicator.style.height = `${indicatorSize}px`;
+
+            indicator.style.borderRadius = "50%";
+
+            indicator.style.left = `${activeItemRect.left + (activeItemRect.width / 2) - (indicator.offsetWidth / 2)}px`;
+
+            indicator.style.bottom = `35px`;
+        }
+    }
+
+    updateIndicator();
+
+    navItems.forEach((item) => {
+        item.addEventListener("click", function () {
+            navItems.forEach((el) => el.classList.remove("active"));
+            item.classList.add("active");
+            updateIndicator();
+        });
+    });
+
+    window.addEventListener("resize", updateIndicator);
+});
+
 
 // Formular
 

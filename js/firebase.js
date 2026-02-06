e8d2e2ccc362277767694e286bc02d49b8f4a867
iurcoegor <iurcoegor@gmail.com>
2026-02-06 18:21:34 +0000

    Chore: Secure Firebase API key

--- js/firebase.js
+++ js/firebase.js
@@ -4,17 +4,8 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.8.0/firebase
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDkLId9RhXjPqL3tb6mX1ZC3BXhfeUkCk0",
-  authDomain: "scoala-de-arte-criuleni.firebaseapp.com",
-  projectId: "scoala-de-arte-criuleni",
-  storageBucket: "scoala-de-arte-criuleni.firebasestorage.app",
-  messagingSenderId: "55607629161",
-  appId: "1:55607629161:web:b981b3f0d293fa251c19f8",
-  measurementId: "G-243PZJ5L6Y"
-};
+// The firebaseConfig object is now defined in js/env.js
+// which is loaded before this script in the HTML.
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);

e4c1c23e0568342427d6d8c8b2ed1f7db03f1f22
iurcoegor <iurcoegor@gmail.com>
2026-02-06 18:46:30 +0000

    Secured the key

--- js/firebase.js
+++ js/firebase.js
@@ -1,11 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-analytics.js";
+import { firebaseConfig } from './firebase-config.js';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
-// The firebaseConfig object is now defined in js/env.js
-// which is loaded before this script in the HTML.
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
--- js/forms.js
+++ js/forms.js
@@ -1,16 +1,7 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
 import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-firestore.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDkLId9RhXjPqL3tb6mX1ZC3BXhfeUkCk0",
-  authDomain: "scoala-de-arte-criuleni.firebaseapp.com",
-  projectId: "scoala-de-arte-criuleni",
-  storageBucket: "scoala-de-arte-criuleni.firebasestorage.app",
-  messagingSenderId: "55607629161",
-  appId: "1:55607629161:web:b981b3f0d293fa251c19f8",
-  measurementId: "G-243PZJ5L6Y"
-};
+import { firebaseConfig } from './firebase-config.js';
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);

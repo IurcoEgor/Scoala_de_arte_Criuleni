622df1911c162f61b2c955102cc138342e75c13a
IurcoEgor <160418653+IurcoEgor@users.noreply.github.com>
2026-02-20 15:43:18 +0000

    Deploying to gh-pages from @ IurcoEgor/Scoala_de_arte_Criuleni@d9ddc5d678eee9c954b975501a921e24ffdee620 🚀

--- js/firebase.js
+++ js/firebase.js
@@ -1,11 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-analytics.js";
-import { firebaseConfig } from './firebase-config.js';
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+const firebaseConfig = {
+  apiKey: "AIzaSyDkLId9RhXjPqL3tb6mX1ZC3BXhfeUkCk0",
+  authDomain: "scoala-de-arte-criuleni.firebaseapp.com",
+  projectId: "scoala-de-arte-criuleni",
+  storageBucket: "scoala-de-arte-criuleni.firebasestorage.app",
+  messagingSenderId: "55607629161",
+  appId: "1:55607629161:web:b981b3f0d293fa251c19f8",
+  measurementId: "G-243PZJ5L6Y"
+};
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
